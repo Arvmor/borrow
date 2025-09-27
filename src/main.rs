3faa802e61a4cbb20820b1feee9b8ef6c7ad4023
Arvin Moravej <arvin.morawej@gmail.com>
2025-09-27 19:40:05 -0400

    feat: Add intervals

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
+use std::time::Duration;
+
 mod api;
 
-fn main() {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     // Init tracing
     tracing_subscriber::fmt::init();
     tracing::info!("Starting the program");
 
-    // Get the vault
-    let vault = api::get_vault("9745", "14");
-    tracing::info!("API Vault: {:?}", vault);
-    println!("{:?}", vault);
+    let mut interval = tokio::time::interval(Duration::from_secs(5));
+    loop {
+        // Get the vault
+        match api::get_vault("9745", "14") {
+            Ok(vault) => tracing::info!("API Vault: {vault:?}"),
+            Err(e) => tracing::error!("Error getting vault: {e:?}"),
+        }
+
+        // Wait for the interval
+        interval.tick().await;
+    }
+
+    Ok(())
 }
